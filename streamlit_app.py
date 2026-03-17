61f35914825ed21df8106959aa0acd3ad84a3858
manssourv <abdessamad.manssour@viseo.com>
2026-03-17 13:07:50 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,8 +35,8 @@ my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_ord
 
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response)
-
+#st.text(smoothiefroot_response.json())
+sf_df= st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 # st.write(my_insert_stmt)
 # st.stop()
 
